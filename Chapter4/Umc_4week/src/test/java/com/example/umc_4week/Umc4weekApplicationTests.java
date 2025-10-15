f1611f22f8d5acb1bfd12b01cae4dd423a1ebdc6
hyukkimm <163751877+hyukkimm@users.noreply.github.com>
2025-10-15 19:17:36 +0900

    Add files via upload

---
+++ Chapter4/Umc_4week/src/test/java/com/example/umc_4week/Umc4weekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umc_4week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Umc4weekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
