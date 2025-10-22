f0e400e8465f464ad6f61197d36ba73f52872b73
hyukkimm <163751877+hyukkimm@users.noreply.github.com>
2025-10-22 19:34:18 +0900

    Add files via upload

---
+++ Repository/ReviewRepository.java
@@ -0,0 +1,26 @@
+package com.example.umc.Repository;
+
+import com.example.umc.Entity.Review;
+import com.example.umc.Entity.Store;
+import com.example.umc.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    @Transactional
+    @Modifying
+    @Query(value = "INSERT INTO review(user_id, store_id, star, content, created_at) VALUES (:userId, :storeId, :star, :content, :createdAt)", nativeQuery = true)
+    void insertReview(
+            @Param("userId") Long userId,
+            @Param("storeId") Long storeId,
+            @Param("star") Float star,
+            @Param("content") String content,
+            @Param("createdAt") LocalDateTime createdAt
+    );
+}
